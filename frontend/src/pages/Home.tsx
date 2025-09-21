f7e01c3077c082ad4c0496a0d042f93c2be956b9
Gabriel Soares Ribeiro <gabrielsoaresribeirotrabalho@gmail.com>
2025-09-21 18:45:43 -0400

    Add tailwindcss classes to stylizing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 function Home() {
   return (
-    <>
-    </>
+    <div className="flex flex-col items-center justify-center gap-6 py-10">
+      <h1 className="text-5xl text-center px-6 py-6 rounded-2xl w-md max-w-4xl mb-3">Página Inicial</h1>
+      <p className="text-3xl text-center px-1 py-1 rounded-2xl w-md max-w-7xl mb-3">Trabalho de Redes de Computadores II</p>
+      <p className="text-xl text-center px-6 py-6 rounded-2xl w-md max-w-7xl mb-3">Sistema de Multimídia<dialog></dialog></p>
+    </div>
   );
 }
 
