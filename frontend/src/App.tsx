6d1c4bb80f1c612a907b6bff3327d8c79e0e63fb
Gabriel Soares Ribeiro <gabrielsoaresribeirotrabalho@gmail.com>
2025-09-21 18:42:37 -0400

    Add tailwindcss classes for stylizing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Player from './components/Player';
+import Streaming from './pages/Streaming';
 
 function App() {
   return (
     <Router>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/stream" element={<Player />} />
-        </Routes>
-      </main>
-      <footer>
-        <Footer />
-      </footer>
+      <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 via-white to-blue-100">
+        <header className="sticky top-0 z-50 shadow bg-white">
+          <Navbar />
+        </header>
+        <main className="flex justify-center container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={
+              <Home />
+            } />
+            <Route path="/stream" element={
+              <Streaming />
+            } />
+          </Routes>
+        </main>
+        <div className="bg-white shadow-inner mt-auto">
+          <Footer />
+        </div>
+      </div>
     </Router>
   )
 }
