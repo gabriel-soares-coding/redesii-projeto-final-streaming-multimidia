cdec110e39e9c5cf90bd79e015ca4fed0a440a28
Gabriel Soares Ribeiro <gabrielsoaresribeirotrabalho@gmail.com>
2025-09-21 18:43:59 -0400

    Adds a visible text more meaningful to the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <div>
-      My Footer
-    </div>
+    <footer className="text-center py-4 bg-white border-t shadow-inner text-">
+      Redes de Computadores II, 2025/01, Discente: Gabriel Soares Ribeiro.
+    </footer>
   );
 }
 
