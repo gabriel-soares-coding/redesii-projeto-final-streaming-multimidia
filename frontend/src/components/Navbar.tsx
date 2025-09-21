ac0b53542e0aaf10dbdcacfaab8b8a9dd89bb637
Gabriel Soares Ribeiro <gabrielsoaresribeirotrabalho@gmail.com>
2025-09-21 18:44:34 -0400

    Add tailwindcss classes to stylizing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const navLinkActiveClasses = "rounded-md bg-sky-900 px-3 py-2 text-lg font-bold text-white mx-3";
+  const navLinkInactiveClasses = "rounded-md px-3 py-2 text-lg font-medium text-black hover:bg-gray-100";
+
+
   return (
-    <nav className="">
-      <NavLink className={({isActive}) => isActive ? "active" : ""} to="/" end>Home</NavLink>
-      <NavLink className={({isActive}) => isActive ? "active" : ""} to="/stream" end>Streaming</NavLink>
+    <nav className="flex items-center justify-center px-6 py-4 bg-white shadow-md">
+      <NavLink className={({ isActive }) => isActive ? "active".concat(" " + navLinkActiveClasses) : navLinkInactiveClasses} to="/">Home</NavLink>
+      <NavLink className={({ isActive }) => isActive ? "active".concat(" " + navLinkActiveClasses) : navLinkInactiveClasses} to="/stream">Streaming</NavLink>
     </nav>
   );
 }
